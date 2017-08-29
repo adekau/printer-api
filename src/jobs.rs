8e5f240ea5ee63a10642995fb0f17ee89fe30960
Alex Dekau <alex.potatofarms@gmail.com>
2017-08-29 16:21:31 -0400

    Integrate host auth data from main into the job runner.

--- src/jobs.rs
+++ src/jobs.rs
@@ -9,11 +9,12 @@ use postgres::{Connection, TlsMode};
 use api;
 use auth_key::{AuthKey, AuthKeyStatus};
 
-pub fn job_runner(available_hosts: Arc<Mutex<Vec<String>>>, config: Config, tx: ThreadOut<String>) {
+pub fn job_runner(available_hosts: Arc<Mutex<Vec<String>>>,
+host_auth: Arc<Mutex<Vec<AuthKey>>>, config: Config, tx: ThreadOut<String>) {
 
     thread::spawn(move || {
-        // Setup the authentication.
-        auth_setup(available_hosts, config).expect("Did not setup properly");
+        // Setup the authentication. Note it will panic if something errors.
+        auth_setup(available_hosts, host_auth, config).expect("Did not setup properly");
 
         loop {
             tx.send("hello world".to_string()).ok();
@@ -28,7 +29,8 @@ pub fn job_runner(available_hosts: Arc<Mutex<Vec<String>>>, config: Config, tx: 
 // Step 2: For each host, check if the hosts already have keys in the database.
 // Step 3: If true: auth_check
 //         If false: generate the key then auth_check.
-fn auth_setup (available_hosts: Arc<Mutex<Vec<String>>>, config: Config) -> io::Result<()> {
+fn auth_setup (available_hosts: Arc<Mutex<Vec<String>>>, 
+host_auth: Arc<Mutex<Vec<AuthKey>>>, config: Config) -> io::Result<()> {
     let conf = config.clone();
     let conn = Connection::connect("postgres://Alex@127.0.0.1:5432", TlsMode::None)?;
 
@@ -45,6 +47,9 @@ fn auth_setup (available_hosts: Arc<Mutex<Vec<String>>>, config: Config) -> io::
             // Extract the information from the database.
             for row in &p {
                 let (host, id, key): (String, String, String) = (row.get(3), row.get(4), row.get(5));
+                let insert = AuthKey::new(host.clone(), id.clone(), key.clone());
+                let mut data = host_auth.lock().unwrap();
+                (*data).push(insert);
                 println!("HOST: {:?} ID: {:?}, KEY: {:?}", host, id, key);
             }
         } else {
@@ -79,6 +84,9 @@ fn auth_setup (available_hosts: Arc<Mutex<Vec<String>>>, config: Config) -> io::
             };
         }
     }
+
+    // Now check the hosts for authorization.
+    api::auth_check_all(host_auth);
 
     // Does this need to return anything? Probably not. TODO: Re-evaluate this later.
     Ok(())
